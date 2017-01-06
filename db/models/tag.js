6d1dc6725e38423483b8683461fa211f090ee4a1
Milad Nazeri <milad@Milads-MacBook-Pro.local>
2017-01-06 15:06:12 -0500

    milad routes

--- db/models/tag.js
+++ db/models/tag.js
@@ -1,14 +1,26 @@
 const Sequelize = require('sequelize')
 const db = require('APP/db')
+const Idea = db.model('ideas')
 
 const Tag = db.define('tags', {
-  name: {
-    type: Sequelize.STRING,
-    unique: true,
-    set: function(value) {
-      this.setDataValue('name', value.toLowerCase())
+    name: {
+      type: Sequelize.STRING,
+      unique: true,
+      set: function(value) {
+        this.setDataValue('name', value.toLowerCase())
+      }
+    },
+  },
+  {
+    classMethods: {
+      findByIdea: function(ideaId) {
+        return Idea.findAll({
+          where: {
+           id: ideaId
+          }
+        })
+      },
     }
-  }
-})
+  })
 
 module.exports = Tag
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,24 @@
 'use strict'
 
-const epilogue = require('./epilogue')
+const epilogue = require('../epilogue')
 const db = require('APP/db')
+const Users = db.model('users')
 
-const customUserRoutes = require('express').Router() 
+const customUserRoutes = require('express').Router()
 
 // Custom routes go here.
 
 module.exports = customUserRoutes
 
+customUserRoutes.get('/:id/ideas/', (req,res,next) => {
+  Users.findByIdeas(req.params.id)
+  .then(ideas => res.json(ideas))
+  res.json
+})
+
 // Epilogue will automatically create standard RESTful routes
 const users = epilogue.resource({
-  model: db.model('users'),
+  model: Users,
   endpoints: ['/users', '/users/:id']
 })
 
