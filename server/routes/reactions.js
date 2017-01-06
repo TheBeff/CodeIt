746ba587664b80931d6992fd0192bfded1000a3d
TheBeff <thebeff@gmail.com>
2017-01-06 15:29:20 -0500

    reactions route

--- server/routes/reactions.js
+++ server/routes/reactions.js
@@ -8,7 +8,23 @@ const customReactionRoutes = require('express').Router()
 
 customReactionRoutes.get('/ideas/:ideaId', (req, res, next) => {
 	Reactions.findByIdea(req.params.ideaId)
-	  .then(response => res.send(response))
+	  .then(response => {
+	  	const reactionCount = response.reduce((prev, cur) => {
+	  	  if (cur.name === "meh") {
+	  	    prev.meh++
+	  	    return prev
+	  	  }
+	  	  else if (cur.name === "great") {
+	  	    prev.great++
+	  	    return prev
+	  	  }
+	  	  else if (cur.name === "star") {
+	  	    prev.star++
+	  	    return prev
+	  	  }
+	  	}
+	  	,{meh: 0, great: 0, star:0});
+	  	res.send(reactionCount)})
 	  .catch(next);
 });
 
