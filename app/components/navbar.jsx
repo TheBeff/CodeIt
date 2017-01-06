8b3740cb94f6c10e7824eb0ab954eea6b41d54a0
verasveras <lisaveras@gmail.com>
2017-01-06 16:13:11 -0500

    to merge

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Login from './Login.jsx';
 import WhoAmI from './WhoAmI.jsx';
+import AddPost from './addPost';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 
 /**
  * A simple example of `AppBar` with an icon on the right.
@@ -11,15 +15,51 @@ export default class  Navbar extends React.Component {
 
 	constructor(props){
 		super(props);
+		this.state = {
+			open: false
+		}
 	}
 
+	handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
 	render(){
+		const actions = [
+	      <FlatButton
+	        label="Cancel"
+	        primary={true}
+	        onTouchTap={this.handleClose}
+	      />,
+	      <FlatButton
+	        label="Submit"
+	        primary={true}
+	        keyboardFocused={true}
+	        onTouchTap={this.handleClose}
+	      />,
+	    ];
+
+
 		return (<AppBar
 	    title="CodeIt"
 	    iconClassNameRight="muidocs-icon-navigation-expand-more">
 	     <nav>
 	    	{this.props.user ? <WhoAmI/> : <Login/>}
 	 	 </nav> 
+	 	 <RaisedButton label="Add Idea" primary={true}/>
+	 	 <Dialog
+          title="Add a new idea"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+        The actions in this window were passed in as an array of React objects.
+        </Dialog>
 	    </AppBar> )
 	}
 
