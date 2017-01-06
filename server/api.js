df8e70d19ae7eb5da9597b44475d15b2eafc01c9
verasveras <lisaveras@gmail.com>
2017-01-06 14:41:51 -0500

    Fixed small bugs

--- server/api.js
+++ server/api.js
@@ -9,10 +9,10 @@ api.use('/comments', require('./routes/users'))
 api.use('/reactions', require('./routes/users'))
 api.use('/ideas', require('./routes/users'))
 
-api
-  .get('/heartbeat', (req, res) => res.send({ok: true,}))
-  .use('/auth', require('./auth'))
-  .use('/users', require('./users'))
+// api
+//   .get('/heartbeat', (req, res) => res.send({ok: true,}))
+//   .use('/auth', require('./auth'))
+//   .use('/users', require('./users'))
 
 // Send along any errors
 api.use((err, req, res, next) => {
